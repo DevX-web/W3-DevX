6f207d1a0b9e9df78a53a1a3ffb44dde87b12a4a
YashKandalkar <yashsk.yk@gmail.com>
2021-02-05 22:16:22 +0530

    chore: add firebase, react-router, small project structure, redux

---
+++ src/firebase/init.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBjUvhGPjZuQvvXMqQ8vR5CsPjXmsGmRnA",
+    authDomain: "apscript-d77eb.firebaseapp.com",
+    projectId: "apscript-d77eb",
+    storageBucket: "apscript-d77eb.appspot.com",
+    messagingSenderId: "688487857543",
+    appId: "1:688487857543:web:51c10a0d20606f418b2722",
+    measurementId: "G-P4TDWC5DTY",
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export { firebaseApp };
