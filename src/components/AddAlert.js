e99d656afa7ece99b4d4b5fb39a8b35610500687
j23saw <jeshsawant23@gmail.com>
2021-02-06 15:40:17 +0530

    add: alert submission

---
+++ src/components/AddAlert.js
@@ -0,0 +1,35 @@
+import {Input, Spacer, Dot, Text, Code, Button} from '@geist-ui/react'
+import {useState} from 'react'
+import {useForm} from 'react-hook-form'
+
+const AddAlert = () => {
+    // const [value, setValue] = useState()
+    // const handler = (e) => {
+    //   setValue(e.target.value)
+    //   console.log(e.target.value)
+    // }
+
+    const { register, handleSubmit, errors } = useForm({ criteriaMode: "all" });
+
+    const onSubmit = (data) => {console.log(data)}
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <Input placeholder="Evil Rabbit" width="50%" name="Name" ref={register({ required: true })}>
+                
+            </Input>
+            <Spacer />
+            <Input placeholder="Post title" width="50%" name="Address" ref={register({ required: true })}>
+                
+            </Input>
+            <Spacer />
+            <Input placeholder="GitHub Actions" width="50%" name="Details" ref={register({ required: true })}>
+
+            </Input>
+            <Spacer/>
+            <Button htmlType="submit">Submit</Button>
+        </form>
+    )
+}
+
+export default AddAlert
